fc87aa12381b655384aeca64a037d2423ba7eb95
echeng8 <dr.frolicks@gmail.com>
2024-10-12 18:02:40 -0700

    refactor/tracklock-fetch-in-own-class

--- Projects/match-data-scraper/Program.cs
+++ Projects/match-data-scraper/Program.cs
@@ -2,31 +2,8 @@
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Newtonsoft.Json; 
+using Newtonsoft.Json;
+using match_data_scraper;
 
-await FetchAndParseDataAsync("https://tracklock.gg/players/94516027"); 
+await TrackLockFetcher.FetchAndParseDataAsync("https://tracklock.gg/players/94516027"); 
 
-async Task FetchAndParseDataAsync(string url)
-{
-    // todo;
-    using HttpClient client = new(); 
-    try
-    {
-        var response = await client.GetStringAsync(url);
-        //Console.WriteLine(response);
-        // \{[^{}]*\"is_win\\"[^{}]*\}
-        //string pattern = @"\{[^{}]*\"is_win\\"[^{}]*\}";
-        string pattern =   @"\{[^{}]*\""is_win\\\""[^{}]*\}";
-        Regex regex = new Regex(pattern, RegexOptions.Multiline);
-
-        MatchCollection matches = regex.Matches(response);
-        foreach(Match match in matches)
-        {
-            Console.WriteLine(match.Value);
-        }
-        Console.WriteLine($"Found {matches.Count} matches");
-    } catch (Exception e)
-    {
-        Console.WriteLine(e.Message);
-    }
-}
---
+++ Projects/match-data-scraper/TrackLockFetcher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace match_data_scraper
+{
+    /// <summary>
+    /// Fetches match data from TrackLock.gg profile url
+    /// This is intended to be a placeholder before we switch to the Deadlock API.
+    /// </summary>
+    public class TrackLockFetcher
+    {
+        public static async Task FetchAndParseDataAsync(string url)
+        {
+            // todo;
+            using HttpClient client = new(); 
+            try
+            {
+                var response = await client.GetStringAsync(url);
+                //Console.WriteLine(response);
+                // \{[^{}]*\"is_win\\"[^{}]*\}
+                //string pattern = @"\{[^{}]*\"is_win\\"[^{}]*\}";
+                string pattern =   @"\{[^{}]*\""is_win\\\""[^{}]*\}";
+                Regex regex = new Regex(pattern, RegexOptions.Multiline);
+
+                MatchCollection matches = regex.Matches(response);
+                foreach(Match match in matches)
+                {
+                    Console.WriteLine(match.Value);
+                }
+                Console.WriteLine($"Found {matches.Count} matches");
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
